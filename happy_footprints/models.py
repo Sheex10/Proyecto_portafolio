880f602b42bebd1caf5b829c5b1e22a3538647a8
Patreyesa <pat.reyesa@duocuc.cl>
2024-04-24 17:21:45 -0400

    otros masssssssss
    JAJAJAJAJAJJA

--- happy_footprints/models.py
+++ happy_footprints/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+#-------------------------------------
+
+class Rol(models.Model):
+    id_rol=models.IntegerField(primary_key=True, verbose_name="id de rol")
+    nombreRol=models.CharField(max_length=30, verbose_name="nombre del rol")
+    
+    def __str__(self):
+        return self.nombreRol
+
+#-----------------------------------------------------------------------
+
+class Usuario(models.Model):
+    id_usuario =models.IntegerField(primary_key=True, verbose_name="id del usuario")
+    nombre=models.CharField(max_length=20, verbose_name="nombre del usuario")
+    apellido=models.CharField(max_length=20, verbose_name="apellido usuario")
+    correo=models.CharField(max_length=50, verbose_name="correo usuario")
+    clave=models.CharField(max_length=12, verbose_name="clave usuario")
+    telefono=models.IntegerField(verbose_name="telefono usuario")
+    rol =models.ForeignKey(Rol,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.correo
+    
+#----------------------------------
+class Categoria(models.Model):
+    id_categoria=models.IntegerField(primary_key=True, verbose_name="id de rol")
+    nombre=models.CharField(max_length=30, verbose_name="nombre de categoria")
+    
+    def __str__(self):
+        return self.nombre
+    
+#--------------------------------------
+class Producto(models.Model):
+    id_producto =models.IntegerField(primary_key=True, verbose_name="id del producto")
+    nombre=models.CharField(max_length=30, verbose_name="nombre del producto")
+    descripcion=models.CharField(max_length=700, verbose_name="descripcion producto")
+    precio=models.IntegerField(verbose_name="precio producto")
+    stock = models.IntegerField()
+    foto=models.ImageField(upload_to="foto producto")
+    categoria =models.ForeignKey(Categoria,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
+
+
--- happy_footprints/views.py
+++ happy_footprints/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Producto
+from .models import Producto, Categoria
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def home(request):
@@ -32,3 +34,23 @@ def buscar_interno_producto(request, id):
 def Producto(request):
 
     return render(request, 'happy_footprints/Producto.html')
+
+
+@login_required
+def formProductos(request):
+    vIdProd = request.POST['id']
+    vDescripcion = request.POST['desc']
+    vPrecio = request.POST['precio']
+    vFoto = request.FILES['foto']
+    vCategoria = request.POST['categoria']
+    vStock = request.POST['stock']
+    vNombre = request.POST['nombre']
+    vRegCategoria = Categoria.objects.get(id_categoria=vCategoria)
+
+    Producto.objects.create(nombre=vNombre, id_producto=vIdProd, descripcion=vDescripcion,
+                            precio=vPrecio, foto=vFoto, categoria=vRegCategoria, stock=vStock)
+
+    if vRegCategoria.id_categoria == 4:
+        return redirect('Perros')
+    if vRegCategoria.id_categoria == 3:
+        return redirect('Gatos')
