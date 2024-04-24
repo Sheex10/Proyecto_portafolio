38369fe701de6ccafd36d42fc75470f67ef54c5b
Patreyesa <pat.reyesa@duocuc.cl>
2024-04-24 17:12:57 -0400

    mientrasssssss
    cambiosssssss

--- happy_footprints/urls.py
+++ happy_footprints/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from happy_footprints import views
-from .views import home
+from .views import home, Gatos, Perros, buscar_interno_producto, Producto
 
 
 
 urlpatterns = [
     path('', home, name="home"),
-
+    path('Gatos', Gatos,name="Gatos"),
+    path('Perros', Perros,name='Perros'),
+    path('buscar_interno_producto/<int:id>',buscar_interno_producto,name="buscar_interno_producto"),
+    path('Producto', Producto, name="Producto"),
 ]
--- happy_footprints/views.py
+++ happy_footprints/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render, redirect
+from .models import Producto
 
 # Create your views here.
 def home(request):
     return render(request, 'happy_footprints/home.html')
+
+def Perros(request):
+    listaProductos = Producto.objects.filter(categoria=1)
+    contexto = {
+        "nombreProd" : listaProductos
+
+    }
+    return render(request, 'happy_footprints/Perros.html', contexto)
+
+
+def Gatos(request):
+    listaProductos = Producto.objects.filter(categoria=2)
+    contexto = {
+        "nombreProd" : listaProductos
+
+    }
+    return render(request, 'happy_footprints/Gatos.html', contexto)
+
+def buscar_interno_producto(request, id):
+    prod = Producto.objects.get(id_producto=id)
+    contexto = {
+        "nombree": prod
+    }
+    return render(request, 'happy_footprints/CamaPerro.html', contexto)
+
+def Producto(request):
+
+    return render(request, 'happy_footprints/Producto.html')
