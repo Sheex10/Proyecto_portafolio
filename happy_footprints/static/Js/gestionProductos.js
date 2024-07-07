60e47a8fca3d87c5737367b4e8ce559e03c19bc8
Felipe <feliperivas1709@gmail.com>
2024-07-06 22:07:22 -0400

    xdd

--- happy_footprints/static/Js/gestionProductos.js
+++ happy_footprints/static/Js/gestionProductos.js
@@ -4,7 +4,7 @@
 
     btnEliminacion.forEach(btn => {
         btn.addEventListener('click', (e) => {
-            const confirmacion = confirm('¿Seguro de eliminar el curso?');
+            const confirmacion = confirm('¿Seguro de eliminar el producto?');
             if (!confirmacion) {
                 e.preventDefault();
             }
